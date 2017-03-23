5bb772c375437f61600898da2ad0c897b1eb9dba
Kython Shu <kythonshu@gmail.com>
2017-03-23 14:08:22 +0800

    Initial Commit

---
+++ FontDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FontDemo
+//
+//  Created by Kython Shu on 2017/3/23.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FontDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FontDemo
+//
+//  Created by Kython Shu on 2017/3/23.
+//  Copyright © 2017年 Kython Shu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
